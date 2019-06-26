877af8175bfac9c8b31af53f1fdcb3574a3048c0
Thomas Harmeyer <thomasmharmeyer@gmail.com>
2019-06-26 11:06:50 -0500

    Completed the Service

---
+++ EntityFrameworkExample/service/BarrelService.cs
@@ -0,0 +1,35 @@
+﻿using EntityFrameworkExample.Models;
+using EntityFrameworkExample.Repository;
+
+namespace EntityFrameworkExample.Controllers
+{
+    internal class BarrelService
+    {
+
+        private BarrelRepository repo;
+        public BarrelService()
+        {
+            repo = new BarrelRepository();
+        }
+
+        public void Create(Barrel barrel)
+        {
+            repo.Create(barrel);
+        }
+
+        public void Edit(Barrel barrel)
+        {
+            repo.Edit(barrel);
+        }
+
+        public void Remove(int? id)
+        {
+            repo.Remove(id);
+        }
+        public Barrel Find(int? id)
+        {
+            return repo.Find(id);
+
+        }
+    }
+}
