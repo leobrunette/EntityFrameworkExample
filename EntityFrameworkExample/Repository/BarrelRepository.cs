b1cd2271bb3c167a94b79ca52c7a09654e1b1603
Leo Brunette <leorbrunette@gmail.com>
2019-06-26 09:56:23 -0500

    Created Repository

---
+++ EntityFrameworkExample/Repository/BarrelRepository.cs
@@ -0,0 +1,17 @@
+﻿using EntityFrameworkExample.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EntityFrameworkExample.Repository
+{
+    public class BarrelRepository
+    {
+        private DataContext dbContext;
+        public BarrelRepository()
+        {
+            dbContext = new DataContext();
+        }
+    }
+}
